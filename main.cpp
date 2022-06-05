ea1d282216efd87cc057f22ec649e9a56e4ca849
jasonchu36 <66146064+jasonchu36@users.noreply.github.com>
2022-06-04 18:11:03 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-int main(){
+int main()
+{
+  ifstream customerFile("data4customer.txt");
+  ifstream moviesFile("data4movies.txt");
+  ifstream transactionsFile("data4commands.txt");
+
+    if (!customerFile)                
+        cout << "Customer file cannot be oppened" << endl;
+    if (!moviesFile)                   
+        cout << "Movie file cannot be oppened" << endl;
+    if (!transactionsFile)             
+        cout << "Transaction file cannot be oppened" << endl;
 }
