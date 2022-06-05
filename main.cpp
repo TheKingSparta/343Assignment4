4b6a615c43a1c05cee9cb67668450659177fc887
Zach Clow <zachclow@uw.edu>
2022-06-04 18:17:39 -0700

    Add code to open the necessary files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main(){
+
+   //Open files
+   ifstream customerFile("data4customers.txt");
+   if(!customerFile) {
+      cout << "Error, data4customers.txt not found";
+      return 1;
+   }
+
+   ifstream movieFile("data4movies.txt");
+   if(!customerFile) {
+      cout << "Error, data4movies.txt not found";
+      return 1;
+   }
+
+   ifstream commandFile("data4commands.txt");
+   if(!customerFile) {
+      cout << "Error, data4commands.txt not found";
+      return 1;
+   }
+
+   
 }
